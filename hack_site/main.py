ba8efa86ba3adc755fc348e2eb1622b3825a1107
Dexter Storey <dexterstorey@Dexters-MacBook-Pro-2.local>
2018-02-03 17:24:21 -0500

    test

--- hack_site/main.py
+++ hack_site/main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Welcome world!')
+        self.response.write('Test world!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
