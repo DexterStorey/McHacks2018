acbaf6a375c01f88c2c561ed65aa9e72eccb6b74
hoynecaleb <hoynecaleb@gmail.com>
2018-02-03 17:49:39 -0500

    Caleb

--- hack_site/main.py
+++ hack_site/main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Test world!')
+        self.response.write('Bitch it works!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
