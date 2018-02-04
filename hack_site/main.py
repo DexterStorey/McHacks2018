d3a1f16e620dacdabf4404b30a6f0da83b6415bf
Dexter Storey <dexterstorey@Dexters-MacBook-Pro-2.local>
2018-02-03 21:17:38 -0500

    added google speech to text api

--- hack_site/main.py
+++ hack_site/main.py
@@ -1,30 +1,32 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
+import io
+import os
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Dexter smells really not good!')
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+# Instantiates a client
+client = speech.SpeechClient()
 
+# The name of the audio file to transcribe
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'resources',
+    'audio.raw')
 
-print('Dex smells like eggs')
+# Loads the audio into memory
+with io.open(file_name, 'rb') as audio_file:
+    content = audio_file.read()
+    audio = types.RecognitionAudio(content=content)
 
+config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code='en-US')
 
+# Detects speech in the audio file
+response = client.recognize(config, audio)
+
+for result in response.results:
+    print('Transcript: {}'.format(result.alternatives[0].transcript))
